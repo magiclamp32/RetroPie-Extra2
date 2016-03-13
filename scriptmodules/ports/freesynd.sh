ecc3764a95a1cc515599e9c239fcdc6f1ddc9274
zerojay <jay@zerojay.com>
2016-03-13 16:34:24 +0000

    Added freesynd script.

---
+++ scriptmodules/ports/freesynd.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="freesynd"
+rp_module_desc="freesynd - Syndicate Engine"
+rp_module_menus="4+"
+rp_module_flags="!x86 !mali"
+
+function depends_freesynd() {
+    getDepends libsdl1.2-dev libsdl-mixer1.2-dev subversion libsdl-image1.2-dev libpng12-dev cmake
+}
+
+function sources_freesynd() {
+    svn checkout svn://svn.code.sf.net/p/freesynd/code/ freesynd-code
+}
+
+function build_freesynd() {
+    cd "$md_build/freesynd-code/freesynd/tags/release-0.7.1"
+    cmake . -DCMAKE_INSTALL_PREFIX:PATH="$md_inst"
+    make
+    md_ret_require="$md_build/freesynd-code/freesynd/tags/release-0.7.1/src/freesynd"
+}
+
+function install_freesynd() {
+    cd "$md_build/freesynd-code/freesynd/tags/release-0.7.1"
+    md_ret_files=(
+        'freesynd-code/freesynd/tags/release-0.7.1/src/freesynd'
+        'freesynd-code/freesynd/tags/release-0.7.1/data'
+    )
+}
+
+function configure_freesynd() {
+    mkRomDir "ports"
+    mkRomDir "ports/freesynd"
+    moveConfigDir "$home/.freesynd" "$configdir/freesynd"
+    cp "$md_build/freesynd-code/freesynd/tags/release-0.7.1/freesynd.ini" "$configdir/$md_id"
+    sed -i "s/fullscreen = false/fullscreen = true/" "$configdir/$md_id/freesynd.ini"
+    sed -i "s/#freesynd_data_dir = \/usr\/share\/freesynd\/data/freesynd_data_dir = \/opt\/retropie\/ports\/freesynd\/data/" "$configdir/$md_id/freesynd.ini"
+    sed -i "s/#data_dir = \/home\/username\/dosbox\/synd\/DATA/data_dir = \/home\/pi\/RetroPie\/roms\/ports\/freesynd/" "$configdir/$md_id/freesynd.ini"
+    addPort "$md_id" "freesynd" "FreeSynd - Syndicate Engine" "$md_inst/freesynd" 
+}
