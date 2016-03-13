57c937e76fce442e56132faae7f7d315ef34a87e
zerojay <jay@zerojay.com>
2016-03-13 14:09:28 -0300

    Swap internal "easyrpg" name with "easyrpgplayer".

--- scriptmodules/ports/easyrpgplayer.sh
+++ scriptmodules/ports/easyrpgplayer.sh
@@ -9,21 +9,21 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="easyrpg"
+rp_module_id="easyrpgplayer"
 rp_module_desc="EasyRPG Player - RPG Maker 2000 and 2003 Interpreter"
 rp_module_menus="4+"
 rp_module_flags="!x86 !mali"
 
-function depends_easyrpg() {
+function depends_easyrpgplayer() {
     getDepends libsdl2-dev libsdl2-mixer-dev libpng12-dev libfreetype6-dev libboost-dev libpixman-1-dev zlib1g-dev autoconf automake libicu-dev
 }
 
-function sources_easyrpg() {
+function sources_easyrpgplayer() {
     gitPullOrClone "$md_build/liblcf" https://github.com/EasyRPG/liblcf.git 
     gitPullOrClone "$md_build/player" https://github.com/EasyRPG/Player.git
 }
 
-function build_easyrpg() {
+function build_easyrpgplayer() {
     cd liblcf
     autoreconf -i
     ./configure --prefix "$md_inst"
@@ -42,7 +42,7 @@ function build_easyrpg() {
     md_ret_require="$md_build/player/easyrpg-player"
 }
 
-function install_easyrpg() {
+function install_easyrpgplayer() {
     cd liblcf/
     make install
 
@@ -50,7 +50,7 @@ function install_easyrpg() {
     make install
 }
 
-function configure_easyrpg() {
+function configure_easyrpgplayer() {
     mkRomDir "ports"
     mkRomDir "ports/$md_id"
     mkRomDir "ports/$md_id/data/"
@@ -58,7 +58,7 @@ function configure_easyrpg() {
     mkRomDir "ports/$md_id/data/rtp2003"
     mkRomDir "ports/$md_id/games/"
 
-    addPort "$md_id" "easyrpg" "EasyRPG Player - RPG Maker 2000 and 2003 Interpreter" "cd $romdir/ports/$md_id/games/; RPG2K_RTP_PATH=$romdir/ports/$md_id/data/rtp2000/ RPG2K3_RTP_PATH=$romdir/ports/$md_id/data/rtp2003/ $md_inst/bin/easyrpg-player"
+    addPort "$md_id" "easyrpgplayer" "EasyRPG Player - RPG Maker 2000 and 2003 Interpreter" "cd $romdir/ports/$md_id/games/; RPG2K_RTP_PATH=$romdir/ports/$md_id/data/rtp2000/ RPG2K3_RTP_PATH=$romdir/ports/$md_id/data/rtp2003/ $md_inst/bin/easyrpg-player"
 
     __INFMSGS+=("You need to unzip your RPG Maker games into subdirectories in $romdir/ports/$md_id/games. Obtain the translated RPG Maker 2000 RTP by Don Miguel and extract it to $romdir/ports/$md_id/data/rtp2000. Obtain the translated RPG Maker 2003 RTP by Advocate and extract it to $romdir/ports/$md_id/data/rtp2003/.")
 }
