3e513133b5158e68976d6ae2f6a38d2613155d21
zerojay <jay@zerojay.com>
2016-03-19 19:29:40 -0300

    Minor typo fix for install script.

--- install-extras.sh
+++ install-extras.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -R scriptmodules/ports/* "/home/pi/RetroPie-Setup/scriptmodules/ports/"
-cp -R scriptmodules/emulators/* "home/pi/RetroPie-Setup/scriptmodules/emulators"
+cp -R scriptmodules/emulators/* "/home/pi/RetroPie-Setup/scriptmodules/emulators"
