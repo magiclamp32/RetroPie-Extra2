b84ccf374481d355ac717ae6188ec89c9f5a4bdf
zerojay <jay@zerojay.com>
2016-03-29 02:35:24 -0300

    Update kaiten-patissier.sh

--- scriptmodules/ports/kaiten-patissier.sh
+++ scriptmodules/ports/kaiten-patissier.sh
@@ -43,7 +43,7 @@ function install_kaiten-patissier() {
 
 function configure_kaiten-patissier() {
     mkRomDir "ports"
-    moveConfigDir "$md_inst/save" "$configdir/$md_id"
+    moveConfigDir "$md_inst/save" "$md_conf_root/$md_id"
     chown -R $user:$user "$md_inst/save"
 
     addPort "$md_id" "kaiten-patissier" "Kaiten Patissier - RotateGear" "pushd $md_inst; $md_inst/RotateGear; popd"
