ba23d73017a491601c635087b4b0aaa8f0d61c73
zerojay <jay@zerojay.com>
2016-04-18 01:03:30 -0300

    Fix glshim checking for bloboats.

--- scriptmodules/ports/bloboats.sh
+++ scriptmodules/ports/bloboats.sh
@@ -19,20 +19,22 @@ function depends_bloboats() {
 }
 
 function sources_bloboats() {
-    if [ ! -a "/opt/retropie/supplementary/glshim/libGL.so.1" ]; then
+    if [ ! -f "/opt/retropie/supplementary/glshim/libGL.so.1" ]; then
         gitPullOrClone "$md_build/glshim" https://github.com/ptitseb/glshim.git
     fi
 }
  
 function build_bloboats() {
-    cd "$md_build/glshim"
-    cmake . -DBCMHOST=1
-    make GL
+    if [ ! -f "/opt/retropie/supplementary/glshim/libGL.so.1" ]; then
+       cd "$md_build/glshim"
+       cmake . -DBCMHOST=1
+       make GL
+    fi
 }
 
 function install_bloboats() {
     aptInstall bloboats
-    if [ ! -a "/opt/retropie/supplementary/glshim/libGL.so.1" ]; then
+    if [ ! -f "/opt/retropie/supplementary/glshim/libGL.so.1" ]; then
        mkdir -p /opt/retropie/supplementary/glshim/
        cp "$md_build/glshim/lib/libGL.so.1" /opt/retropie/supplementary/glshim/
     fi
