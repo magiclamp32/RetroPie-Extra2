c25d62c5ed5e4144797657f2e5d0f3180bd27122
Scottt <scott.geyer@eagles.usm.edu>
2017-02-26 12:38:27 -0600

    Funnyboat
    A side scrolling boat shooter with waves, sharks, and pirates

---
+++ scriptmodules/ports/funnyboat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="funnyboat"
+rp_module_desc="Funny Boat. A side scrolling boat shooter with waves."
+rp_module_section="exp"
+rp_module_flags="!mali !x86"
+
+function install_bin_seahorse() {
+    aptInstall funnyboat
+}
+
+function configure_funnyboat() {
+    mkRomDir "ports"
+    moveConfigDir "$home/.funnyboat" "$md_conf_root/$md_id"
+
+    addPort "$md_id" "funnyboat" "Funny Boat" "funnyboat"
+}
