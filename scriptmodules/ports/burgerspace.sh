dcc1accee247659355e6f726b7957b38591ee745
zerojay <jay@zerojay.com>
2017-07-23 13:57:52 -0400

    Burgerspace - Added module help.

--- scriptmodules/ports/burgerspace.sh
+++ scriptmodules/ports/burgerspace.sh
@@ -11,6 +11,7 @@
 
 rp_module_id="burgerspace"
 rp_module_desc="BurgerSpace - BurgerTime clone"
+rp_module_help="Because this uses X, you may find that you are unable to control the game and the game appears in a small window in the top left. Use the Runcommand option to set the resolution to CEA-4 or similarly smaller sizes. This will allow you to control the game as the window will have focus and also fill up more of the screen."
 rp_module_licence="GPL2 https://www.gnu.org/licenses/gpl-2.0.txt"
 rp_module_section="exp"
 rp_module_flags="!mali !x86"
