cefc571ceec563ed1f51d6d2f529e0696d66780b
zerojay <jay@zerojay.com>
2017-07-23 12:35:07 -0400

    Updated for Allegro5 dependencies
    Updated the file manifest, starting the game fails still but this is better than it was.
    Partial fix for #218. Might be a good idea for @martinohanlon to take a look.

--- scriptmodules/ports/mayhem.sh
+++ scriptmodules/ports/mayhem.sh
@@ -16,7 +16,7 @@ rp_module_section="exp"
 rp_module_flags="!x86 !mali"
 
 function depends_mayhem() {
-    getDepends liballegro4.4 liballegro4-dev xinit
+    getDepends liballegro5.0 liballegro5-dev liballegro-audio5-dev liballegro-acodec5-dev liballegro-image5-dev liballegro-ttf5-dev libdumb1 pulseaudio xinit
 }
 
 function sources_mayhem() {
@@ -32,39 +32,13 @@ function build_mayhem() {
 function install_mayhem() {
     md_ret_files=(
 	'mayhem2-pi'
-	'sfx_boom.WAV'
-        'sfx_loop_refuel.WAV'
-        'sfx_loop_shield.WAV'
-        'sfx_loop_thrust.WAV'
-        'sfx_rebound.WAV'
-        'sfx_shoot.WAV'
-        'ship1_256c.bmp'
-        'ship1_shield_256c.bmp'
-        'ship1_thrust_256c.bmp'
-        'ship2_256c.bmp'
-        'ship2_shield_256c.bmp'
-        'ship2_thrust_256c.bmp'
-        'ship3_256c.bmp'
-        'ship3_shield_256c.bmp'
-        'ship3_thrust_256c.bmp'
-        'ship4_256c.bmp'
-        'ship4_shield_256c.bmp'
-        'ship4_thrust_256c.bmp'
-        'Mayhem_Level1_Map_256c.bmp'
-        'Mayhem_Level2_Map_256c.bmp'
-        'Mayhem_Level3_Map_256c.bmp'
-        'Mini_map1.bmp'
-        'Mini_map2.bmp'
-        'Mini_map3.bmp'
-        'Option.bmp'
-        'Sprite_explosion.bmp'
-        'intro_logo.bmp'
-        'mayhem.jpg'
-        'start'
+	'assets'
+        'mayhem2.jpg'
+        'start.sh'
     )
 }
 
 function configure_mayhem() {
     mkRomDir "ports"
-    addPort "$md_id" "mayhem" "Mayhem - Remake of Amiga Game" "pushd $md_inst; ./start; popd"
+    addPort "$md_id" "mayhem" "Mayhem - Remake of Amiga Game" "pushd $md_inst; ./start.sh; popd"
 }
