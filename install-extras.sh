f6a06ea762d3a47228a311065c6f0cc15d7ee18e
zerojay <jay@zerojay.com>
2017-11-13 21:39:09 -0500

    Add the libretrocores to the install script.
    Thanks to jonnykesh.

--- install-extras.sh
+++ install-extras.sh
@@ -3,3 +3,4 @@
 cp -R scriptmodules/ports/* "/home/pi/RetroPie-Setup/scriptmodules/ports/"
 cp -R scriptmodules/emulators/* "/home/pi/RetroPie-Setup/scriptmodules/emulators"
 cp -R scriptmodules/supplementary/* "/home/pi/RetroPie-Setup/scriptmodules/supplementary"
+cp -R scriptmodules/libretrocores/* "/home/pi/RetroPie-Setup/scriptmodules/libretrocore"
