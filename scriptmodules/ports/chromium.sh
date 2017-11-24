aa6fed83d3d8006105f66c2a6820096743ad2bd6
zerojay <jay@zerojay.com>
2017-11-24 13:10:23 -0500

    Install rpi-chromium-mods as a dependency of Chromium on current Raspbian versions only.
    Fix for issue #255.

--- scriptmodules/ports/chromium.sh
+++ scriptmodules/ports/chromium.sh
@@ -17,7 +17,12 @@ rp_module_section="exp"
 rp_module_flags="!mali !x86"
 
 function depends_chromium() {
-    getDepends git omxplayer libgnome-keyring-common libgnome-keyring0 libnspr4 libnss3 xdg-utils matchbox xserver-xorg-legacy xorg gconf-service libgconf-2-4
+    if [[ "$__raspbian_ver" -lt 8 ]]; then
+        getDepends git omxplayer libgnome-keyring-common libgnome-keyring0 libnspr4 libnss3 xdg-utils matchbox xserver-xorg-legacy xorg gconf-service libgconf-2-4
+    else
+        getDepends git omxplayer libgnome-keyring-common libgnome-keyring0 libnspr4 libnss3 xdg-utils matchbox xserver-xorg-legacy xorg gconf-service libgconf-2-4 rpi-chromium-mods
+    fi
+    
 }
 
 function sources_chromium() {
