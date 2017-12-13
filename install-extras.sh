377c751d662e1ae9fa727a44e75ea77aa93a8a57
MileSacul <11881208+lashower@users.noreply.github.com>
2017-12-13 09:23:00 -0500

    Update install-extras.sh

--- install-extras.sh
+++ install-extras.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-RPS_HOME='/home/pi/RetroPie-Setup/'
+RPS_HOME='/home/pi/RetroPie-Setup'
 if [ ! -z "$1" ];then
-    echo $1
+    RPS_HOME="$1"
 fi
-cp -R scriptmodules/* $RPS_HOME
+if [ ! -d "$RPS_HOME" ];then
+    echo "RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup."
+    echo "EX: ./install-extras.sh /home/pi/RetroPie-Setup"
+    exit
+fi
+
+echo "Placing scriptmodules in $RPS_HOME"
+cp -R scriptmodules/* $RPS_HOME/scriptmodules
