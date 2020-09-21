b36a2440cdb53ba9b9018c2096aec553656b6694
Jay Carter <jay@zerojay.com>
2020-09-21 08:12:44 -0400

    Wizznic is now provided as part of Buster, so just install it instead of compiling it.

--- scriptmodules/ports/wizznic.sh
+++ scriptmodules/ports/wizznic.sh
@@ -15,31 +15,9 @@ rp_module_section="exp"
 rp_module_flags="!x86 !mali"
 
 function depends_wizznic() {
-    getDepends libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev
-}
- 
-function sources_wizznic() {
-    gitPullOrClone "$md_build" https://github.com/DusteDdk/Wizznic.git
-}
- 
-function build_wizznic() {
-    make -f Makefile.linux
-    md_ret_require="$md_build/wizznic"
-}
- 
-function install_wizznic() {
-    md_ret_files=(
-        'wizznic'
-        'data'
-        'doc'
-        'packs'
-    )
+    aptInstall wizznic
 }
  
 function configure_wizznic() {
-    mkRomDir "ports"
-    touch "$md_inst/settings.ini"
-    moveConfigFile "$md_inst/settings.ini" "$md_conf_root/$md_id/settings.ini"
-    chown -R $user:$user "$md_conf_root/$md_id"
-    addPort "$md_id" "wizznic" "Wizznic - Puzznic clone" "pushd $md_inst; $md_inst/wizznic -sw; popd"
+    addPort "$md_id" "wizznic" "Wizznic - Puzznic clone" "/usr/games/wizznic -z 2 -sw"
 }
