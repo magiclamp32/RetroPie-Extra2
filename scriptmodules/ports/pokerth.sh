9c98e5ef8cca05b95e187965eab4c0226287f303
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-01-11 13:06:03 -0600

    Create pokerth.sh

---
+++ scriptmodules/ports/pokerth.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="pokerth"
+rp_module_desc="pokerth is an open source online poker"
+rp_module_licence="https://github.com/GNOME/epiphany"
+rp_module_section="exp"
+rp_module_flags="!mali !x86"
+
+function depends_pokerth() {
+    getDepends xorg matchbox
+}
+
+function install_bin_pokerth() {
+    aptInstall pokerth
+}
+
+function configure_pokerth() {
+    mkRomDir "ports"
+    mkdir -p "$md_inst"
+    moveConfigDir "$home/.config" "$md_conf_root/$md_id"
+    cat >"$md_inst/pokerth.sh" << _EOF_
+#!/bin/bash
+xset -dpms s off s noblank
+matchbox-window-manager -use_titlebar no &
+/usr/games/pokerth
+_EOF_
+    chmod +x "$md_inst/pokerth.sh"
+    
+     addPort "$md_id" "pokerth" "PokerTH is an Open Source Online Poker" "xinit $md_inst/pokerth.sh"
+}
