62c206aceceef67c12c77f46674531e55c96316f
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-01-11 14:24:26 -0600

    adding new/updated scripts

---
+++ scriptmodules/ports/hurrican.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="hurrican"
+rp_module_desc="Hurrican - Turrican clone - A freeware jump and shoot game made by Poke53280 (Eiswuxe)"
+rp_module_help="After installation, select game from the Ports system.\n\nSelect gamepad controls in game preferences menu."
+rp_module_repo="git https://github.com/drfiemost/Hurrican.git master b8b3d31"
+rp_module_licence="MIT https://raw.githubusercontent.com/drfiemost/Hurrican/master/LICENSE"
+rp_module_section="exp"
+rp_module_flags="!all rpi4"
+
+function depends_hurrican() {
+    getDepends libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libepoxy-dev cmake
+}
+
+function sources_hurrican() {
+    gitPullOrClone
+}
+
+function build_hurrican() {
+    cd "$md_build/Hurrican"
+    mkdir build && cd build
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    make
+    md_ret_require="$md_build/Hurrican/build/hurrican"
+}
+
+function install_hurrican() {
+    md_ret_files=(
+        'Hurrican/build/hurrican'
+        'Hurrican/data'
+        'Hurrican/lang'
+        'Hurrican/splashscreen.bmp'
+        'Editor/maps'
+        'Editor/Gfx'
+    )
+}
+
+function configure_hurrican() {
+    addPort "$md_id" "hurrican" "Hurrican - Turrican clone" "pushd $md_conf_root/hurrican; $md_inst/hurrican --pathdata $md_inst --pathsave $md_conf_root/hurrican; popd"
+    mkRomDir "ports"
+    moveConfigDir "$home/.config/hurrican" "$md_conf_root/hurrican"
+}
--- scriptmodules/supplementary/screenshot.sh
+++ scriptmodules/supplementary/screenshot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
- 
+
+# adapted from ZeroJay's RetroPie-Extra:
+# https://github.com/zerojay/RetroPie-Extra
+
 # This file is part of The RetroPie Project
 #
 # The RetroPie Project is the legal property of its developers, whose names are
@@ -8,28 +11,28 @@
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
- 
+
 rp_module_id="screenshot"
 rp_module_desc="Universal Screenshot with Raspi2PNG"
+rp_module_help="Usage: 'screenshot [destination]' over SSH. File saved to '$datadir/screenshots/(date)_(time).png' if no destination given.\n\nThis script is incompatible with the OpenGL driver."
+rp_module_repo="git https://github.com/AndrewFromMelbourne/raspi2png.git master b3c5599"
 rp_module_licence="MIT https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/LICENSE"
-rp_module_help="To take a screenshot use $datadir/screenshots/screenshot.sh over SSH.\n\nThis script is incompatible with the OpenGL driver."
 rp_module_section="exp"
- 
+
 function depends_screenshot() {
     getDepends libpng-dev
 }
- 
+
 function sources_screenshot() {
-    gitPullOrClone "$md_build" https://github.com/AndrewFromMelbourne/raspi2png.git
+    gitPullOrClone
 }
- 
+
 function build_screenshot() {
-    cd "$md_build"
     make clean
     make
-    md_ret_require="$md_build"
+    md_ret_require="$md_build/raspi2png"
 }
- 
+
 function install_screenshot() {
     md_ret_files=(
         'raspi2png'
@@ -37,16 +40,32 @@ function install_screenshot() {
         'README.md'
     )
 }
- 
-function configure_screenshot() {
-    mkUserDir "$datadir/screenshots"
- 
+
+function script_screenshot() {
     # Create script to take screenshot over ssh
-    cat > "$datadir/screenshots/screenshot.sh" << _EOF_
+    cat > "$md_inst/$md_id.sh" << _EOF_
 #!/bin/bash
-$rootdir/supplementary/screenshot/raspi2png -p $datadir/screenshots/\$(date +%m%d%Y_%H%M%S).png
+dest="\$1"
+[[ ! -n "\$dest" ]] && dest="$datadir/screenshots/\$(date +%Y%m%d_%H%M%S).png"
+[[ -d "\$dest" ]] && dest="\$dest/\$(date +%Y%m%d_%H%M%S).png"
+dest_fileext="\${dest##*.}"
+[[ "\${dest_fileext,,}" != "png" ]] && dest="\$dest.png"
+$md_inst/raspi2png -p "\$dest" && echo Saved "\$dest"
 _EOF_
- 
-    chown $user:$user "$datadir/screenshots/screenshot.sh"
-    chmod +x "$datadir/screenshots/screenshot.sh"
+
+    chmod +x "$md_inst/$md_id.sh"
+    ln -sf "$md_inst/$md_id.sh" /usr/local/bin/screenshot
+}
+
+function configure_screenshot() {
+    if [[ "$md_mode" == "remove" ]]; then
+        [[ -h /usr/local/bin/screenshot ]] && rm -f /usr/local/bin/screenshot
+        remove_share_samba "screenshots"
+        restart_samba
+        return
+    fi
+    mkUserDir "$datadir/screenshots"
+    script_screenshot
+    add_share_samba "screenshots" "$datadir/screenshots"
+    restart_samba
 }
