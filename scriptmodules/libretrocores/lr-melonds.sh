a903cbe90fcfde00fb1908e4f75aeb66e2d41510
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-01-11 12:55:18 -0600

    Create lr-melonds.sh

---
+++ scriptmodules/libretrocores/lr-melonds.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-melonds"
+rp_module_desc="NDS emu - MelonDS port for libretro"
+rp_module_help="ROM Extensions: .nds .zip .7z\n\nCopy your Nintendo DS roms to $romdir/nds\n\nCopy firmware.bin, bios7.bin and bios9.bin to $biosdir"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/melonDS/master/LICENSE"
+rp_module_repo="git https://github.com/libretro/melonDS.git master"
+rp_module_section="exp"
+rp_module_flags=""
+
+#function depends_lr-melonds() {
+#    getDepends libwebkitgtk-3.0-dev libcurl4-gnutls-dev libpcap0.8-dev libsdl2-dev
+#}
+
+function sources_lr-melonds() {
+    gitPullOrClone
+}
+
+function build_lr-melonds() {
+    make clean
+    make
+    md_ret_require="$md_build/melonds_libretro.so"
+}
+
+function install_lr-melonds() {
+    md_ret_files=(
+        'melonds_libretro.so'
+    )
+}
+
+function configure_lr-melonds() {
+    mkRomDir "nds"
+    ensureSystemretroconfig "nds"
+
+    addEmulator 0 "$md_id" "nds" "$md_inst/melonds_libretro.so"
+    addSystem "nds"
+}
