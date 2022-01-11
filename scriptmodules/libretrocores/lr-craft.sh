8953f4a1cf01efd4ab9964d5c1d5f239db50dda6
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-01-11 12:53:09 -0600

    Create lr-craft.sh

---
+++ scriptmodules/libretrocores/lr-craft.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-craft"
+rp_module_desc="Minecraft engine - Craft port for libretro"
+rp_module_licence="MIT https://raw.githubusercontent.com/libretro/Craft/master/LICENSE.md"
+rp_module_repo="git https://github.com/libretro/Craft master"
+rp_module_section="exp"
+rp_module_flags=""
+
+function sources_lr-craft() {
+    gitPullOrClone
+}
+
+function build_lr-craft() {
+    make -f Makefile.libretro clean
+    make -f Makefile.libretro
+    md_ret_require="$md_build/craft_libretro.so"
+}
+
+function install_lr-craft() {
+    md_ret_files=(
+        'craft_libretro.so'
+    )
+}
+
+function configure_lr-craft() {
+    setConfigRoot "ports"
+
+    addPort "$md_id" "craft" "Craft" "$md_inst/craft_libretro.so"
+
+    ensureSystemretroconfig "ports/craft"
+}
