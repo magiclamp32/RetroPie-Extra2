c5a2a244a1193f05aa0657d4647f16a63f56120c
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-01-17 10:38:56 -0600

    Update nxengine.sh
    updating fixes from  @s1eve-mcdichae1

--- scriptmodules/ports/nxengine.sh
+++ scriptmodules/ports/nxengine.sh
@@ -9,47 +9,45 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="nxengine"
+rp_module_id="nxengine-evo"
 rp_module_desc="Cave Story engine clone - NXEngine-Evo"
+rp_module_help="NXEngine by Caitlin Shaw, refactoring by isage et al."
 rp_module_licence="GPL3 http://nxengine.sourceforge.net/LICENSE"
-rp_module_help=""
-rp_module_section="opt"
+rp_module_repo="git https://github.com/nxengine/nxengine-evo.git"
+rp_module_section="exp"
 rp_module_flags="!armv6 !mali"
 
-function depends_nxengine() {
-   sudo apt install build-essential libpng-dev libjpeg-dev make cmake cmake-data git libsdl2-dev libsdl2-doc libsdl2-gfx-dev libsdl2-gfx-doc libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev 
+function depends_nxengine-evo() {
+    getDepends libpng-dev libjpeg-dev cmake libsdl2-dev libsdl2-gfx-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev 
 }
 
-function sources_nxengine() {
-   git clone https://github.com/nxengine/nxengine-evo.git
+function sources_nxengine-evo() {
+    gitPullOrClone
 }
 
-function build_nxengine() {
-     cd nxengine-evo
-     mkdir build 
-     cd build
-     cmake -DCMAKE_BUILD_TYPE=Release -Wno-dev -DCMAKE_INSTALL_PREFIX=/home/pi/RetroPie/roms/ports/CaveStory ..
-     make
+function build_nxengine-evo() {
+    mkdir build 
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -Wno-dev -DCMAKE_INSTALL_PREFIX="$romdir/ports/CaveStory" ..
+    make
 
-     cd ..
-     wget "https://www.cavestory.org/downloads/cavestoryen.zip"
-     unzip cavestoryen.zip
-     cp -r CaveStory/data/ ./
-	cp -r CaveStory/Doukutsu.exe ./
+    cd ..
+    downloadAndExtract "https://www.cavestory.org/downloads/cavestoryen.zip"
+    cp -r cavestoryen/CaveStory/data .
+    cp cavestoryen/CaveStory/Doukutsu.exe .
 
-    wget "https://github.com/nxengine/translations/releases/download/v1.14/all.zip"
-    mkdir translations && unzip all.zip -d translations
-    cp -r translations/data ./
+    downloadAndExtract "https://github.com/nxengine/translations/releases/download/v1.14/all.zip" "translations"
+    cp -r translations/data .
 
-	build/nxextract
+    build/nxextract
 }
 
-function install_nxengine() {
-     cd nxengine-evo/build
-	sudo make install
+function install_nxengine-evo() {
+    cd "$md_build/build"
+    make install
 }
 
-function configure_nxengine() {
-    addPort "$md_id" "cavestory" "Cave Story" "/home/pi/RetroPie/roms/ports/CaveStory/bin/nxengine-evo"
-       chown -R $user:$user "/home/pi/RetroPie/roms/ports/CaveStory"
+function configure_nxengine-evo() {
+    addPort "$md_id" "cavestory" "Cave Story" "$romdir/ports/CaveStory/bin/nxengine-evo"
+    chown -R $user:$user "$romdir/ports/CaveStory"
 }
