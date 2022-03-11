74e02a8f00beaf67a857976f415ccda0d1fac687
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-03-10 18:42:06 -0800

    (Tidying)
    (Simply reordered for consistency with other modules: addPort, makeRomDir, moveConfigDir)

--- scriptmodules/ports/shadowwarrior.sh
+++ scriptmodules/ports/shadowwarrior.sh
@@ -36,7 +36,7 @@ function install_shadowwarrior() {
 }
 	
 function configure_shadowwarrior() {
+    addPort "$md_id" "sw" "Jfsw - Shadow Warrior source port" "XINIT: $md_inst/sw"
     mkRomDir "ports/shadowwarrior"
     moveConfigDir "$home/.jfsw" "$md_conf_root/shadowwarrior"
-    addPort "$md_id" "sw" "Jfsw - Shadow Warrior source port" "XINIT: $md_inst/sw"
 }
