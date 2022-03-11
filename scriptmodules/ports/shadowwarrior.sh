02e52426a4e4a24a7581b837af2af6b1e184eead
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-03-10 18:12:04 -0800

    build-essential and git are hard deps.
    (They are necessarily already installed.)

--- scriptmodules/ports/shadowwarrior.sh
+++ scriptmodules/ports/shadowwarrior.sh
@@ -19,7 +19,7 @@ rp_module_section="exp"
 rp_module_flags=""
 
 function depends_shadowwarrior() {
-    getDepends cmake build-essential libgl1-mesa-dev libgtk2.0-dev libsdl1.2-dev libvorbis-dev timidity freepats git
+    getDepends cmake libgl1-mesa-dev libgtk2.0-dev libsdl1.2-dev libvorbis-dev timidity freepats
 }
 
 function sources_shadowwarrior() {
