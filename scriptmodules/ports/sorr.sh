139420c7e3988f529b39f543cd4b95ac73f40660
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-03-15 07:56:17 -0500

    Update sorr.sh

--- scriptmodules/ports/sorr.sh
+++ scriptmodules/ports/sorr.sh
@@ -16,7 +16,7 @@ rp_module_section="exp"
 rp_module_flags="!x86 !x11 !mali"
 
 function depends_sorr() {
-    getDepends libsdl-mixer1.2 libpng12-0
+    getDepends libsdl-mixer1.2 libpng12-0 xorg
 }
 
 function sources_sorr() {
