05e0ae7ccba11253766271ca64cdfc8e704d28ac
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-04-05 14:04:17 -0700

    Expand module desc.

--- scriptmodules/ports/supertuxkart.sh
+++ scriptmodules/ports/supertuxkart.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="supertuxkart"
-rp_module_desc="SuperTuxKart"
+rp_module_desc="SuperTuxKart - a free kart-racing game featuring the Linux mascot Tux the Penguin, inspired by the popular Mario Kart series of racers."
 rp_module_licence="GPL3 https://sourceforge.net/p/supertuxkart/code/HEAD/tree/main/trunk/COPYING?format=raw"
 rp_module_section="exp"
 rp_module_flags="!mali !x86"
