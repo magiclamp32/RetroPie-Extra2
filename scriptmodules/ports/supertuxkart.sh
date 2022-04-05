b5f5b9d464042c9418f91a912a1ba04f741bbfac
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-04-05 13:40:43 -0700

    Cleaned up spacing

--- scriptmodules/ports/supertuxkart.sh
+++ scriptmodules/ports/supertuxkart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
- 
+
 # This file is part of The RetroPie Project
 #
 # The RetroPie Project is the legal property of its developers, whose names are
@@ -8,31 +8,31 @@
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
- 
+
 rp_module_id="supertuxkart"
 rp_module_desc="SuperTuxKart"
 rp_module_licence="GPL3 https://sourceforge.net/p/supertuxkart/code/HEAD/tree/main/trunk/COPYING?format=raw"
 rp_module_section="exp"
 rp_module_flags="!mali !x86"
- 
+
 function depends_supertuxkart() {
     getDepends xorg
 }
 
 function install_bin_supertuxkart() {
-	[ -f "$md_inst/supertuxkart" ]] && rm -rf "$md_inst/"*
-	aptInstall supertuxkart
+    [ -f "$md_inst/supertuxkart" ]] && rm -rf "$md_inst/"*
+    aptInstall supertuxkart
 }
- 
- function remove_supertuxkart() {
+
+function remove_supertuxkart() {
     aptRemove supertuxkart
 }
 
 function configure_supertuxkart() {
-    addPort "$md_id" "supertuxkart" "SuperTuxKart" "XINIT:$md_inst/supertuxkart.sh"	
-	
-	
-	cat >"$md_inst/supertuxkart.sh" << _EOF_
+    addPort "$md_id" "supertuxkart" "SuperTuxKart" "XINIT:$md_inst/supertuxkart.sh"
+
+
+    cat >"$md_inst/supertuxkart.sh" << _EOF_
 #!/bin/bash
 /usr/games/supertuxkart
 _EOF_
