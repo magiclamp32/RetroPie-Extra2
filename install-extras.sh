98451f6a0c2d13877cb24eddd11b7cfc6c0fe8eb
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-04-09 23:38:13 -0700

    Update install-extras.sh
    Minor update to mirror the new remove script.

--- install-extras.sh
+++ install-extras.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 RPS_HOME="$HOME/RetroPie-Setup"
-if [ ! -z "$1" ];then
+if [ ! -z "$1" ]; then
     RPS_HOME="$1"
 fi
-if [ ! -d "$RPS_HOME" ];then
-    echo "RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup."
-    echo "EX: ./install-extras.sh /home/pi/RetroPie-Setup"
+RP_EXTRA="$RPS_HOME/ext/RetroPie-Extra"
+
+if [ ! -d "$RPS_HOME" ]; then
+    echo -e "Error: RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup, ex:\n\n    ./install-extras.sh /home/pi/RetroPie-Setup\n\nAborting."
     exit
 fi
 
-echo "Placing scriptmodules in $RPS_HOME"
-mkdir -p "$RPS_HOME/ext/RetroPie-Extra/scriptmodules"
-cp -R scriptmodules/* "$RPS_HOME/ext/RetroPie-Extra/scriptmodules"
+echo "Placing scriptmodules in $RP_EXTRA"
+mkdir -p "$RP_EXTRA"
+cp -R scriptmodules "$RP_EXTRA" && echo -e "...done."
