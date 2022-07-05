cd29bfdc6840a4bf173b20d06e1ced66fef3023b
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-07-05 16:37:54 -0700

    Added (-i)nteractive option
    Use with -i to run GUI menu, or without to install all modules, non-interactively.

--- gui.sh
+++ gui.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+GUI=0
+if [[ "${1,,}" == "-i" ]]; then
+    GUI=1
+    shift
+fi
+readonly GUI
+
 RPS_HOME="$HOME/RetroPie-Setup"
 if [[ -n "$1" ]]; then
     RPS_HOME="$1"
@@ -9,6 +16,13 @@ readonly RP_EXTRA="$RPS_HOME/ext/RetroPie-Extra"
 
 if [[ ! -d "$RPS_HOME" ]]; then
     echo -e "Error: RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup, ex:\n\n    ./install-extras.sh /home/pi/RetroPie-Setup\n\nAborting."
+    exit
+fi
+
+if [[ "$GUI" -eq 0 ]]; then
+    echo "Placing scriptmodules in $RP_EXTRA"
+    mkdir -p "$RP_EXTRA"
+    cp -R scriptmodules/ "$RP_EXTRA" && echo "...done."
     exit
 fi
 
@@ -39,4 +53,5 @@ while true; do
         break
     fi
 done
+
 clear
