8b63260bff96ed971a0c7a678298722c916ba153
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-07-05 21:42:54 -0700

    Renamed gui.sh to install-extras.sh

--- install-extras.sh
+++ install-extras.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
+GUI=0
+if [[ "${1,,}" == "-i" || "${1,,}" == "--interactive" ]]; then
+    GUI=1
+    shift
+fi
+readonly GUI
+
 RPS_HOME="$HOME/RetroPie-Setup"
-if [ ! -z "$1" ]; then
+if [[ -n "$1" ]]; then
     RPS_HOME="$1"
 fi
-RP_EXTRA="$RPS_HOME/ext/RetroPie-Extra"
+readonly RPS_HOME
 
-if [ ! -d "$RPS_HOME" ]; then
-    echo -e "Error: RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup, ex:\n\n    ./install-extras.sh /home/pi/RetroPie-Setup\n\nAborting."
+readonly RP_EXTRA="$RPS_HOME/ext/RetroPie-Extra"
+readonly BACKTITLE="Installation utility for RetroPie-Extra - Setup directory: $RPS_HOME"
+
+function startCmd() {
+    if [[ ! -d "$RPS_HOME" ]]; then
+        echo -e "Error: RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup, ex:\n\n    ./$(basename $0) /home/pi/RetroPie-Setup\n\nAborting."
+        exit
+    elif [[ "$GUI" -eq 1 ]]; then
+        runGUI
+    else
+        runAuto
+    fi
+}
+
+function runAuto() {
+    echo "Placing scriptmodules in $RP_EXTRA"
+    mkdir -p "$RP_EXTRA"
+    cp -R scriptmodules/ "$RP_EXTRA" && echo -e "\n...done."
     exit
-fi
+}
 
-echo -e "Placing scriptmodules in $RP_EXTRA"
-mkdir -p "$RP_EXTRA"
-cp -R scriptmodules "$RP_EXTRA" && echo -e "...done."
+function runGUI() {
+    cmd=(dialog --clear --backtitle "$BACKTITLE" --menu "Choose an option." 22 86 16)
+    while true; do
+        local options=(
+            1 "Install all RetroPie-Extra modules"
+            2 "Choose which modules to install"
+        )
+        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        if [[ -n "$choice" ]]; then
+            case "$choice" in
+                1)
+                    local errormsg=$(mkdir -p "$RP_EXTRA" && cp -r scriptmodules "$RP_EXTRA")
+                    if [[ -n "$errormsg" ]]; then
+                        errormsg="Error: $errormsg"
+                    else
+                        errormsg="All scriptmodules copied to $RP_EXTRA."
+                    fi
+                    dialog --backtitle "$BACKTITLE" --cr-wrap --no-collapse --msgbox "$errormsg" 20 60
+                    ;;
+                2)
+                    chooseModules
+                    ;;
+            esac
+        else
+            break
+        fi
+    done
+
+    clear
+}
+
+function chooseModules() {
+    dialog --backtitle "$BACKTITLE" --cr-wrap --no-collapse --msgbox "Coming soon..." 20 60
+}
+
+# Run
+startCmd
