e1da89ca8e73140d7094ff551d53e53af4844b74
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-07-06 10:01:42 -0700

    Adding comment for mysterious 'pkill -CONT'

--- scriptmodules/supplementary/bgm123/bgm_stop.sh
+++ scriptmodules/supplementary/bgm123/bgm_stop.sh
@@ -6,4 +6,7 @@ source #autoconf
 
 vcgencmd force_audio hdmi 0 >/dev/null && sleep 0.2
 pkill "$music_player"
+
+# if killed while stopped (music paused), process will not terminate correctly.
+# restart the stopped process to terminate it:
 [[ "$(ps -ostate= -C $music_player)" == "T" ]] && pkill -CONT "$music_player"
