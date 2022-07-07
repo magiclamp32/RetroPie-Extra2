46c99cbe5366d81dfb0d6536b5ded688ba6dad45
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-07-07 14:49:02 -0700

    WIP
    I think this is functional.  It doesn't have separate sections yet or any confirmation of your choices, but it lets you select them and installs the ones you've selected.

--- install-extras.sh
+++ install-extras.sh
@@ -28,9 +28,9 @@ function startCmd() {
 }
 
 function runAuto() {
-    echo "Placing scriptmodules in $RP_EXTRA"
+    echo -e "Placing scriptmodules in $RP_EXTRA\n"
     mkdir -p "$RP_EXTRA"
-    cp -R scriptmodules/ "$RP_EXTRA" && echo -e "\n...done."
+    cp -R scriptmodules/ "$RP_EXTRA" && echo "...done."
     exit
 }
 
@@ -38,23 +38,23 @@ function runGUI() {
     cmd=(dialog --clear --backtitle "$BACKTITLE" --menu "Choose an option." 22 86 16)
     while true; do
         local options=(
-            1 "Install all RetroPie-Extra modules"
-            2 "Choose which modules to install"
+            1 "Choose which modules to install"
+            2 "Install all RetroPie-Extra modules"
         )
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         if [[ -n "$choice" ]]; then
             case "$choice" in
                 1)
-                    local errormsg=$(mkdir -p "$RP_EXTRA" && cp -r scriptmodules "$RP_EXTRA")
+                    chooseModules
+                    ;;
+                2)
+                    local errormsg=$(mkdir -p $RP_EXTRA 2>&1 && cp -r scriptmodules $RP_EXTRA 2>&1)
                     if [[ -n "$errormsg" ]]; then
                         errormsg="Error: $errormsg"
                     else
                         errormsg="All scriptmodules copied to $RP_EXTRA."
                     fi
                     dialog --backtitle "$BACKTITLE" --cr-wrap --no-collapse --msgbox "$errormsg" 20 60
-                    ;;
-                2)
-                    chooseModules
                     ;;
             esac
         else
@@ -66,7 +66,38 @@ function runGUI() {
 }
 
 function chooseModules() {
-    dialog --backtitle "$BACKTITLE" --cr-wrap --no-collapse --msgbox "Coming soon..." 20 60
+    local options=()
+    local module
+    local i=1
+
+    local cmd=(dialog --backtitle "$BACKTITLE" --checklist "Choose which modules to install:" 22 60 16)
+
+    while read module; do
+        module="${module/scriptmodules\//}"
+        options+=($i "$module" off)
+        ((i++))
+    done < <(find scriptmodules -mindepth 2 -maxdepth 2 -type f | sort -u)
+
+    local choices=($("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty))
+    local choice
+    local script
+    local datadir
+    local section
+    local target
+
+    for choice in "${choices[@]}"; do
+        script="scriptmodules/${options[choice*3-2]}"
+        section="$(basename $(dirname $script))"
+        datadir="${script%.*}"
+        target="$RP_EXTRA/scriptmodules/$section"
+
+        [[ ! -d "$target" ]] && mkdir -p "$target"
+        cp -f "$script" "$target"
+        [[ -d "$datadir" ]] && cp -r "$datadir" "$target"
+    done
+
+    clear
+    exit
 }
 
 # Run
