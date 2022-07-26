8f6de3b037f740550ad628315cc77bdceafd2650
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2022-07-26 08:19:55 -0500

    Update reminiscence.sh
    adding old script back

--- scriptmodules/ports/reminiscence.sh
+++ scriptmodules/ports/reminiscence.sh
@@ -1,43 +1,41 @@
-#!/usr/bin/envbash
+#!/usr/bin/env bash
 
-#This file is part of RetroPie-Extra, a supplement to RetroPie.
-#For more information, please visit:
-#
-#https://github.com/RetroPie/RetroPie-Setup
-#https://github.com/Exarkuniv/RetroPie-Extra
-#
-#See the LICENSE file distributed with this source and at
-#https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/LICENSE
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
 rp_module_id="reminiscence"
-rp_module_desc="REminiscence- Flashback Engine"
-rp_module_licence="GPL3https://raw.githubusercontent.com/Cheeseness/REminiscence/master/COPYING"
-rp_module_help="Pleasecopy your Flashback data files to $romdir/ports/$md_id before running REminiscence."
-rp_module_repo="file http://cyxdown.free.fr/reminiscence/REminiscence-0.2.1.tar.bz2"
+rp_module_desc="REminiscence - Flashback Engine"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/Cheeseness/REminiscence/master/COPYING"
+rp_module_help="Please copy your Flashback data files to $romdir/ports/$md_id before running REminiscence."
 rp_module_section="exp"
 rp_module_flags="!mali !x86"
 
-functiondepends_reminiscence() {
+function depends_reminiscence() {
     getDepends libsdl1.2-dev
 }
 
-functionsources_reminiscence() {
-    downloadAndExtract "$md_repo_url" "$md_build" "--strip-components=1"
+function sources_reminiscence() {
+    wget -O- -q http://cyxdown.free.fr/reminiscence/REminiscence-0.2.1.tar.bz2 | tar -xvj --strip-components=1
 }
 
-functionbuild_reminiscence() {
+function build_reminiscence() {
     make
 }
 
-functioninstall_reminiscence() {
+function install_reminiscence() {
     md_ret_files=('rs')
 }
 
-functionconfigure_reminiscence() {
+function configure_reminiscence() {
     mkRomDir "ports"
     mkRomDir "ports/$md_id"
-   #fixme: Missing configuration dir.
+    #fixme: Missing configuration dir.
 
     addPort "$md_id" "reminiscence" "REminiscence" "$md_inst/rs --datapath=$romdir/ports/$md_id --savepath=$md_conf_root/$md_id"
 }
